c74d31eeddeac7311d247e346570427fd1df062c
vlad_veko <veko.2001@gmail.com>
2021-02-01 23:14:02 +0300

    Implemented search

--- src/routes/news.js
+++ src/routes/news.js
@@ -43,4 +43,24 @@ news.get('/article/:id', async(req, res) => {
     }
 })
 
+news.post('', async(req, res) => {
+    let search = req.params.search
+    try {
+        const newsReq = await axios.get(`https://techcrunch.com/wp-json/wp/v2/posts?search=${search}`)
+        res.render('news', { newsArticles : newsReq.data })
+    } catch (err) {
+        if (err.response) {
+            console.log(err.response.status)
+            console.log(err.response.headers)
+            console.log(err.response.data)
+        }
+        else if (err.request) {
+            console.log(err.request)
+        }
+        else {
+            console.error('Error', err.message)
+        }
+    }
+})
+
 module.exports = news
